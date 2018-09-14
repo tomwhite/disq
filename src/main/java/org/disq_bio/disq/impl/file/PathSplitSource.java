fb80933b7d043d46bdb8f7c5a9bff8234bfeb70d
Tom White <tom.e.white@gmail.com>
2018-09-14 11:42:38 +0100

    Add debug logging

--- src/main/java/org/disq_bio/disq/impl/file/PathSplitSource.java
+++ src/main/java/org/disq_bio/disq/impl/file/PathSplitSource.java
@@ -3,6 +3,7 @@ package org.disq_bio.disq.impl.file;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
@@ -11,10 +12,14 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import scala.Tuple2;
 
 public class PathSplitSource implements Serializable {
+
+  private static final Logger logger = LoggerFactory.getLogger(PathSplitSource.class);
 
   private final FileSystemWrapper fileSystemWrapper;
 
@@ -34,11 +39,13 @@ public class PathSplitSource implements Serializable {
 
       List<Long> range = LongStream.range(0, numSplits).boxed().collect(Collectors.toList());
       return jsc.parallelize(range, numSplits)
-          .map(idx -> idx * splitSize)
-          .flatMap(
-              splitStart -> {
+          .map(
+              idx -> {
+                long splitStart = idx * splitSize;
                 final long splitEnd = splitStart + splitSize > len ? len : splitStart + splitSize;
-                return Collections.singleton(new PathSplit(path, splitStart, splitEnd)).iterator();
+                PathSplit pathSplit = new PathSplit(path, splitStart, splitEnd);
+                logger.debug("PathSplit for partition {}: {}", idx, pathSplit);
+                return pathSplit;
               });
     } else {
       // Use Hadoop FileSystem API to maintain file locality by using Hadoop's FileInputFormat
@@ -49,17 +56,20 @@ public class PathSplitSource implements Serializable {
       }
       return jsc.newAPIHadoopFile(
               path, FileSplitInputFormat.class, Void.class, FileSplit.class, conf)
-          .flatMap(
-              (FlatMapFunction<Tuple2<Void, FileSplit>, PathSplit>)
-                  t2 -> {
+          .mapPartitionsWithIndex(
+              (Function2<Integer, Iterator<Tuple2<Void, FileSplit>>, Iterator<PathSplit>>)
+                  (idx, it) -> {
+                    Tuple2<Void, FileSplit> t2 = it.next(); // one file split per partition
                     FileSplit fileSplit = t2._2();
                     PathSplit pathSplit =
                         new PathSplit(
                             fileSplit.getPath().toString(),
                             fileSplit.getStart(),
                             fileSplit.getStart() + fileSplit.getLength());
+                    logger.debug("PathSplit for partition {}: {}", idx, pathSplit);
                     return Collections.singleton(pathSplit).iterator();
-                  });
+                  },
+              true);
     }
   }
 }
